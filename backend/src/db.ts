5fe454d7dd16f9f642b72d0d853e164daa545482
Devanshukoli <kolidevanshu02@gmail.com>
2026-02-27 12:21:01 +0530

    remove redundant dotenv config call from db.ts file

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/eisenhower-matrix';
-// const MONGODB_URI = 'mongodb://127.0.0.1:27017/eisenhower-matrix';
 
 export const connectDB = async () => {
   try {
